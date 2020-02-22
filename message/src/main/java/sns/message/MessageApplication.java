9e6ca19686401a7310cb925ce20d0723b0d8e6b5
chohanjoo <chohanjoo@github.com>
2020-02-23 00:06:39 +0900

    Add front named face

---
+++ message/src/main/java/sns/message/MessageApplication.java
@@ -0,0 +1,17 @@
+package sns.message;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
+
+@SpringBootApplication
+@ImportResource(value = {"classpath:META-INF/datasource.xml"})
+@MapperScan(value = {"sns.message.dao"})
+public class MessageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessageApplication.class, args);
+	}
+
+}
