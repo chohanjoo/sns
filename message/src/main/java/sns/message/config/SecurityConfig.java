173b9ccaf4e61b4f48d106bf125a23acc86d2be8
chohanjoo <johanjoo@naver.com>
2020-02-25 13:48:11 +0900

    Apply Srping Security

--- message/src/main/java/sns/message/config/SecurityConfig.java
+++ message/src/main/java/sns/message/config/SecurityConfig.java
@@ -1,10 +1,16 @@
 package sns.message.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+//import org.springframework.session.web.http.HeaderHttpSessionStrategy;
+//import org.springframework.session.web.http.HttpSessionStrategy;
 import sns.message.dao.UserDao;
 import sns.message.service.UserSecurityService;
 
@@ -18,10 +24,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.NEVER)
+                .and()
                 .authorizeRequests()
+                .antMatchers("/user/login").permitAll()
+                .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                .antMatchers("/user").hasAuthority("USER")
+                .antMatchers("/post").hasAuthority("USER")
                 .anyRequest().authenticated()
                 .and()
-                .formLogin();
+                .formLogin()
+                .and()
+                .logout();
     }
 
 
@@ -30,4 +44,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         auth.userDetailsService(userSecurityService)
                 .passwordEncoder(userSecurityService.passwordEncoder());
     }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+//    @Bean
+//    public HttpSessionStrategy httpSessionStrategy(){
+//        return new HeaderHttpSessionStrategy();
+//    }
 }
--- message/src/main/java/sns/message/controller/UserController.java
+++ message/src/main/java/sns/message/controller/UserController.java
@@ -4,12 +4,21 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.web.bind.annotation.*;
+import sns.message.domain.AuthenticationToken;
 import sns.message.dto.ProfileDto;
 import sns.message.dto.UserDto;
+import sns.message.request.AuthenticationRequest;
 import sns.message.request.CreateUserRequest;
+import sns.message.service.UserSecurityService;
 import sns.message.service.UserService;
 
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
 @CrossOrigin
@@ -19,6 +28,12 @@ public class UserController {
 
     @Autowired
     UserService userService;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    UserSecurityService userSecurityService;
 
     @ApiOperation(value = "전체 User 가져오기")
     @GetMapping("/user")
@@ -39,5 +54,23 @@ public class UserController {
     @ResponseStatus(value = HttpStatus.OK)
     public ProfileDto retrieveUserProfile(String user_id){
         return this.userService.retrieveUserProfile(user_id);
+    }
+
+    @RequestMapping(value="/user/login", method=RequestMethod.POST)
+    public AuthenticationToken login(
+            @RequestBody AuthenticationRequest authenticationRequest,
+            HttpSession session
+    ) {
+        String username = authenticationRequest.getUsername();
+        String password = authenticationRequest.getPassword();
+
+        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(username, password);
+        Authentication authentication = authenticationManager.authenticate(token);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        session.setAttribute(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY,
+                SecurityContextHolder.getContext());
+
+        UserDto user = userSecurityService.readUser(username);
+        return new AuthenticationToken(user.getId(), user.getAuthorities(), session.getId());
     }
 }
