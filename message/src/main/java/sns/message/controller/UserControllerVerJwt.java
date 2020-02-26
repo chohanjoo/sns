4b0b16066ff11ea2e110ad2c2c35de3875f52bdb
chohanjoo <johanjoo@naver.com>
2020-02-26 20:49:01 +0900

    token 활성화 후 모든 유저 조회

--- message/src/main/java/sns/message/controller/UserControllerVerJwt.java
+++ message/src/main/java/sns/message/controller/UserControllerVerJwt.java
@@ -5,6 +5,7 @@ import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +20,7 @@ import sns.message.service.UserService;
 @RequestMapping(value = "/v1")
 public class UserControllerVerJwt {
 
+    @Autowired
     private UserService userService;
     private final ResponseService responseService; // 결과를 처리할 Service
 
