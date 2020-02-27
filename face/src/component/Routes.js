27ccd9e5488a7d17804c3c62ed7e201a2e1177e7
chohanjoo <johanjoo@naver.com>
2020-02-28 00:30:26 +0900

    Signup Page 추가

--- face/src/component/Routes.js
+++ face/src/component/Routes.js
@@ -2,10 +2,12 @@ import React from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import CardComponent from './PostComponent';
 import SignIn from './SignIn';
+import SignUp from "./SignUp";
 
 export default () => (
     <Router>
         <Route path="/post" component={CardComponent}/>
         <Route path="/user/login" component={SignIn}/>
+        <Route path="/user/signup" component={SignUp}/>
     </Router>
 )
