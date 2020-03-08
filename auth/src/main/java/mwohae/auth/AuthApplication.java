da2b4a1fdf54ed0ed636be42acb15bc370255a77
chohanjoo <johanjoo@naver.com>
2020-03-08 22:37:48 +0900

    모노리틱 아키텍처에서 마이크로서비스 아키텍처로 변경 #13  - Eureka service registry Netflix Oss 추가

--- auth/src/main/java/mwohae/auth/AuthApplication.java
+++ auth/src/main/java/mwohae/auth/AuthApplication.java
@@ -3,11 +3,13 @@ package mwohae.auth;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+@EnableEurekaClient
 @SpringBootApplication
 @ImportResource(value = {"classpath:META-INF/datasource.xml"})
 @MapperScan(value = {"mwohae.auth.dao"})
