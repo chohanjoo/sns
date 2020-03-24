a58a2bc297677006686e88a1551d7cc0c6be121d
chohanjoo <johanjoo@naver.com>
2020-03-24 09:24:08 +0900

    모노리틱 아키텍처에서 마이크로서비스 아키텍처로 변경 #13  - spring profile 분할 (dev, prod)  - 도커 이미지 삭제 쉘 스크립트 작성

---
+++ shell_script/delete_docker_images.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+VERSION='v0.1.2'
+
+echo `docker rmi chohanjoo/mwohae-gateway:$VERSION`
+echo `docker rmi chohanjoo/mwohae-post:$VERSION`
+echo `docker rmi chohanjoo/mwohae-user:$VERSION`
+echo `docker rmi chohanjoo/mwohae-auth:$VERSION`
+echo `docker rmi chohanjoo/mwohae-service-registry:$VERSION`
+echo `docker rmi chohanjoo/mwohae-face:$VERSION`
